c0f7b4efe940fdb695d76b78106a593df9c7a3f6
hitesh0611 <chaudharihitesh0611@gmail.com>
2024-06-07 13:29:08 +0530

    java

---
+++ helloworld.java
@@ -0,0 +1,44 @@
+class helloworld {
+    public static void main(String[] args) {
+        int Money= 2000;
+        System.out.println(Money);
+        Money=5000;
+        System.out.println(Money);
+
+    }
+
+}
+
+//38:07
+
+/
+/46;27
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
